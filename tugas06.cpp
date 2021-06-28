a65b74550118d5adf7e742b94a0515f278a81882
whoisraa <rairaiatlantica@gmail.com>
2021-06-28 20:01:04 +0700

    tugas 6 cpp

---
+++ tugas06.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x,z;
+	cout<<"Inputkan Nilai x : ";
+	cin>>x;
+	cout<<"Inputkan Nilai z : ";
+	cin>>z;
+	if(x>z){
+		cout<<"Hasil : "<<x+z;
+	}else{
+		cout<<"Hasil : "<<x-z;
+	}
+}
